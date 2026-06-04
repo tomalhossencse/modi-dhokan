0bff469d31fb27999cc4cb81325142225f6d6dfd
Tomal Hossen <tomalhossen78@gmail.com>
2026-06-04 16:21:42 +0600

    app Promo Banner added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import AddPromoBanner from "../components/Home/AddPromoBanner";
 import Features from "../components/Home/Features";
 import Hero from "../components/Home/Hero";
 import HomeCategories from "../components/Home/HomeCategories";
@@ -6,10 +7,11 @@ import PopularProducts from "../components/Home/PopularProducts";
 const Home = () => {
   return (
     <div className="min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 ">
-      <Hero  />
+      <Hero />
       <Features />
       <HomeCategories />
       <PopularProducts />
+      <AddPromoBanner />
     </div>
   );
 };
